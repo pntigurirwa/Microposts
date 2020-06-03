282ee4b6e8fbb6c8778206693d7d63ff258bcd70
pntigurirwa <pntigurirwa@gmail.com>
2020-06-03 02:08:16 -0500

    post Edit State & Update(1)

--- src/app.js
+++ src/app.js
@@ -10,10 +10,12 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 // Get Posts
 function getPosts() {
-  http
-    .get('http://localhost:3000/posts')
+  http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
 }
@@ -26,11 +28,10 @@ function submitPost() {
   const data = {
     title,
     body
-  };
+  }
 
   // Create Post
-  http
-    .post('http://localhost:3000/posts', data)
+  http.post('http://localhost:3000/posts', data)
     .then(data => {
       ui.showAlert('Post added', 'alert alert-success');
       ui.clearFields();
@@ -41,17 +42,36 @@ function submitPost() {
 
 // Delete Post
 function deletePost(e) {
-  e.preventDefault();
-  if (e.target.parentElement.classList.contains('delete')) {
+  if(e.target.parentElement.classList.contains('delete')) {
     const id = e.target.parentElement.dataset.id;
-    if (confirm('Are you sure?')) {
-      http
-        .delete(`http://localhost:3000/posts/${id}`)
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
         .then(data => {
-          ui.showAlert('Post Removed', 'alert alert-success');
+          ui.showAlert('Post removed', 'alert alert-success');
           getPosts();
         })
         .catch(err => console.log(err));
     }
   }
+  e.preventDefault();
+}
+
+// Enable Edit State
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post
+    ui.fillForm(data);
+  }
+  
+  e.preventDefault();
 }
